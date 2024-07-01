b4cf9f2a5a930ac8c80ec3689c684a9816d4f662
Olaitan Adeboye <mercyware@gmail.com>
2024-07-01 19:40:30 +0100

    Updated Clean user data

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -24,7 +24,7 @@ class UserService {
             await this.generateAndSendOTP(user.id);
         }
 
-        return this.cleanUserData(user);
+        return cleanUserData(user);
     }
 
     async checkUserExistence(userData) {
